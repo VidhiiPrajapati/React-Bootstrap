d192e1d319fb4d7a4011e8a55358848a3c428c5f
Vidhi Prajapati <prajapatividhi016@gmail.com>
2023-06-08 19:12:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,134 @@
 import logo from './logo.svg';
 import './App.css';
+import { Row, Col, Image, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Card from 'react-bootstrap/Card';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </header>
+
+      <main>
+        <Container>
+          <Row className="px-4 my-5">
+            <Col sm={7}>
+
+              <Image src="https://picsum.photos/900/400" fluid rounded />
+
+
+
+            </Col>
+
+            <Col sm={5}>
+              <h1 class='font-weight=light'>
+                TagLine
+              </h1>
+
+              <p className='mt-4'>Praesent venenatis convallis odio quis lacinia. In vel est fringilla, pellentesque dolor at, rhoncus justo. Donec ullamcorper mi orci, ac interdum mauris cursus vitae
+                . Nunc nec lorem non nisl dapibus rutrum sit amet a magna</p>
+
+              <Button variant="outline-primary"> Call me Action </Button>
+
+            </Col>
+          </Row>
+
+          <Row>
+
+
+            <Card className='text-center bg-secondary text-white my-5 py-4'>
+              <Card.Body>This is some text within a card body.</Card.Body>
+            </Card>
+
+          </Row>
+
+          <Row>
+
+          <Col>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="https://picsum.photos/id/201/320/200" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="https://picsum.photos/id/202/320/200" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="https://picsum.photos/320/200" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          </Row>
+
+        </Container>
+      </main>
+
+      <footer className='py-5 my-5 bg-dark'>
+        <Container className='px-4'>
+          <p className='text-center text-white'>
+
+            CopyRight &copy;Your Website 2023
+
+          </p>
+        </Container>
+
+      </footer>
+
     </div>
   );
 }
